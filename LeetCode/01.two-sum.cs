530bd513fb02f72c5c1bba9938d273203724db35
Akaishi Toshiya <Toshiya14@live.jp>
2024-02-28 04:44:17 +0800

    Init.

---
+++ LeetCode/01.two-sum.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode;
+
+public class SolutionTwoSum
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var indexCache = new Dictionary<int, int>();
+        // Mapping from value to index.
+
+        foreach (var (currentValue, currentIndex) in nums.Select((x, i) => (x, i)))
+        {
+            var otherValue = target - currentValue;
+
+            if (indexCache.TryGetValue(otherValue, out var otherIndex))
+            {
+                // Got the result.
+                var solution = new[] { otherIndex, currentIndex };
+                return solution;
+            }
+            else
+            {
+                // push the index into cache.
+                indexCache[currentValue] = currentIndex;
+            }
+        }
+
+        throw new Exception("Should have at least 1 solution, but got 0.");
+    }
+}
