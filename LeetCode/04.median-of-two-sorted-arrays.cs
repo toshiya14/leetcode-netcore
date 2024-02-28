6644f471591c349ab3ef9ec5a217f54f0bcddab1
Akaishi Toshiya <Toshiya14@live.jp>
2024-02-28 15:28:33 +0800

    Optimize 04.median-of-two-sorted-arrays: binary search to reduce the time cost to log(n+m)

--- LeetCode/04.median-of-two-sorted-arrays.cs
+++ LeetCode/04.median-of-two-sorted-arrays.cs
@@ -14,76 +14,88 @@ public class SolutionMedianOfTwoSortedArrays
 
         var total = m + n;
 
-        int mi1, mi2;
-        if (total % 2 == 0)
+        var i = total / 2;
+        var hasM2 = total % 2 == 0;
+
+        int s1 = 0, s2 = 0;
+
+        if (hasM2)
         {
-            mi1 = total / 2 - 1;
-            mi2 = total / 2;
+            var m1 = GetNthSmallNum(nums1, nums2, i, ref s1, ref s2);
+            //Console.WriteLine($"M1: {m1}, S1: {s1}, S2: {s2}");
+            var m2 = GetNthSmallNum(nums1.AsSpan()[s1..], nums2.AsSpan()[s2..], 1, ref s1, ref s2);
+            //Console.WriteLine($"M2: {m2}, S1: {s1}, S2: {s2}");
+            return (m1 + m2) / 2.0;
         }
         else
         {
-            mi1 = total / 2;
-            mi2 = -1;
+            var m1 = GetNthSmallNum(nums1, nums2, i + 1, ref s1, ref s2);
+            //Console.WriteLine($"M1: {m1}, S1: {s1}, S2: {s2}");
+            return m1;
+        }
+    }
+
+    public static int GetNthSmallNum(Span<int> nums1, Span<int> nums2, int n, ref int s1, ref int s2)
+    {
+        var i = Math.Max(0, n / 2 - 1);
+
+        //Console.WriteLine($"VERBOSE: num1={string.Join(',', nums1.ToArray())}, num2={string.Join(',', nums2.ToArray())}, n={n}");
+
+        if (nums1.Length == 0)
+        {
+            s2 += n;
+            return nums2[n - 1];
         }
 
-        //Console.Write($"mi1={mi1}, mi2={mi2}");
-
-        int i1 = 0, i2 = 0;
-        int m1 = -1, m2 = -1;
-        for (var gi = 0; gi <= mi1 + 1; gi++)
+        if (nums2.Length == 0)
         {
-            int current;
-            var i1Overflow = i1 >= nums1.Length;
-            var i2Overflow = i2 >= nums2.Length;
-            if (i1Overflow && i2Overflow)
+            s1 += n;
+            return nums1[n - 1];
+        }
+
+        if (n == 1)
+        {
+            var n1 = nums1[0];
+            var n2 = nums2[0];
+            if (n1 < n2)
             {
-                break;
-            }
-            else if (i1Overflow)
-            {
-                current = nums2[i2];
-                i2++;
-            }
-            else if (i2Overflow)
-            {
-                current = nums1[i1];
-                i1++;
+                s1 += 1;
+                return n1;
             }
             else
             {
-                var n1 = nums1[i1];
-                var n2 = nums2[i2];
-
-                if (n1 < n2)
-                {
-                    current = n1;
-                    i1++;
-                }
-                else
-                {
-                    current = n2;
-                    i2++;
-                }
+                s2 += 1;
+                return n2;
             }
 
-            if (gi == mi1)
-            {
-                m1 = current;
-            }
-
-            if (gi == mi2)
-            {
-                m2 = current;
-            }
-        }
-
-        if (mi2 < 0)
-        {
-            return m1;
         }
         else
         {
-            return (m1 + m2) / 2.0;
+            int i1 = i, i2 = i;
+            if (i >= nums1.Length)
+            {
+                i1 = nums1.Length - 1;
+            }
+            else if (i >= nums2.Length)
+            {
+                i2 = nums2.Length - 1;
+            }
+            var n1 = nums1[i1];
+            var n2 = nums2[i2];
+            //Console.WriteLine($"VERBOSE: n1={n1}, n2={n2}, i1={i1}, i2={i2}");
+            if (n1 < n2)
+            {
+                s1 += i1 + 1;
+                var k = n - i1 - 1;
+                return GetNthSmallNum(nums1[(i1 + 1)..], nums2, k, ref s1, ref s2);
+            }
+            else
+            {
+                s2 += i2 + 1;
+                var k = n - i2 - 1;
+                return GetNthSmallNum(nums1, nums2[(i2 + 1)..], k, ref s1, ref s2);
+            }
         }
+
     }
 }
--- Tests/04.median-of-two-sorted-arrays.cs
+++ Tests/04.median-of-two-sorted-arrays.cs
@@ -17,9 +17,13 @@ public class TestsMedianOfTwoSortedArrays
     {
         Assert.Multiple(() =>
         {
+            Assert.That(this.Solution.FindMedianSortedArrays([1], [2, 3, 4, 5, 6, 7]), Is.EqualTo(4.0));
+            Assert.That(this.Solution.FindMedianSortedArrays([], [1, 2, 3, 4]), Is.EqualTo(2.5));
+            Assert.That(this.Solution.FindMedianSortedArrays([1, 3], [2, 7]), Is.EqualTo(2.5));
             Assert.That(this.Solution.FindMedianSortedArrays([1, 3], [2]), Is.EqualTo(2.0));
             Assert.That(this.Solution.FindMedianSortedArrays([1, 2], [3, 4]), Is.EqualTo(2.5));
             Assert.That(this.Solution.FindMedianSortedArrays([], [2, 3]), Is.EqualTo(2.5));
+            Assert.That(this.Solution.FindMedianSortedArrays([1, 3, 4, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]), Is.EqualTo(4));
         });
     }
 }
