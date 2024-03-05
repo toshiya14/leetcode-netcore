529fa395f53b27ed5fb6ca324499714ed0de7940
Akaishi Toshiya <Toshiya14@live.jp>
2024-03-05 18:36:37 +0800

    Update 00033.search-in-rotated-sorted-array: ignore the rotation but use normal binary search would be faster.

--- LeetCode/00033.search-in-rotated-sorted-array.cs
+++ LeetCode/00033.search-in-rotated-sorted-array.cs
@@ -9,7 +9,6 @@ public class SolutionSearchInRotatedSortedArray
 {
     public int Search(int[] nums, int target)
     {
-        //Console.WriteLine($"New: {string.Join(',', nums)}, target={target}");
         if (nums.Length == 0)
         {
             return -1;
@@ -20,59 +19,44 @@ public class SolutionSearchInRotatedSortedArray
             return nums[0] == target ? 0 : -1;
         }
 
-        // 1. Find k
-        var k = 0;
-        for (var i = 0; i < nums.Length - 1; i++)
-        {
-            if (nums[i] > nums[i + 1])
-            {
-                k = nums.Length - i - 1;
-            }
-        }
-
-        // 2. Binary Search
-        return BinarySearch(nums, k, 0, nums.Length - 1, target);
+        return BinarySearch(nums, 0, nums.Length - 1, target);
     }
 
-    private static int BinarySearch(int[] nums, int k, int left, int right, int target)
+    private static int BinarySearch(int[] nums, int left, int right, int target)
     {
-        //Console.WriteLine($"BinarySearch: k={k}, l={left}, r={right}");
-        if (left == right)
+        if (right <= left)
         {
-            var realleft = GetRealIndex(left, k, nums.Length);
-            if (target == nums[realleft])
+            if (nums[left] == target)
             {
-                return realleft;
+                return left;
+            }
+            else if (nums[right] == target)
+            {
+                return right;
             }
             else
             {
                 return -1;
             }
         }
-        else if (right < left)
-        {
-            return -1;
-        }
 
         var mid = (left + right) / 2;
-        var realmid = GetRealIndex(mid, k, nums.Length);
-        //Console.WriteLine($"mid: {mid}, realmid: {realmid}");
-        if (nums[realmid] == target)
+        if (nums[mid] == target)
         {
-            return realmid;
-        }
-        else if (nums[realmid] < target)
-        {
-            return BinarySearch(nums, k, mid + 1, right, target);
+            return mid;
         }
         else
         {
-            return BinarySearch(nums, k, left, mid - 1, target);
+            var li = BinarySearch(nums, left, mid, target);
+            if (li < 0)
+            {
+                var ri = BinarySearch(nums, mid + 1, right, target);
+                return ri;
+            }
+            else
+            {
+                return li;
+            }
         }
-    }
-
-    private static int GetRealIndex(int i, int k, int len)
-    {
-        return (len - k + i) % len;
     }
 }
